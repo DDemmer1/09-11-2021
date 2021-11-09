ca50051409f6e49de0d0206ba061e10960ca45b0
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2021-11-09 09:36:43 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -4,6 +4,23 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
+				// Die Aussage soll wahr sein, wenn die Variablen x und y nicht gleich sind
+				// und das Ergebnis von a geteilt durch b gerade ist
+		
+
+				//Hier nichts ändern
+				int x = 5;
+				int y = 4;
+
+				int a = 4;
+				int b = 2;
+				
+				
+				//Hier könnt ihr euren Ausdruck Eintragen
+				boolean bool =   ;
+
+				System.out.println("Der Ausdruck sollte true sein und ist : " + bool);
+		
 	}
 
 }
